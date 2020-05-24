07cbc7d8ae2ff658a2421d6c4654af6d17a875bb
zhanarys <zarasovzanarys110@gmail.com>
2020-05-24 13:19:17 +0600

    файл game.java

---
+++ game.java
@@ -0,0 +1,17 @@
+package game;
+import javax.swing.JOptionPane;
+
+public class game {
+
+	public static void main(String[] args) {
+		
+		String choise = JOptionPane.showInputDialog(null, "Choose the difficulty of the game from 1 to 7","Game difficulty",1);
+		
+		int  hard = choise.charAt(0)-'0';
+		if ((hard>=1)&&(hard<=7)) {
+			okno window = new okno(hard);
+		}
+		
+	}
+
+}
